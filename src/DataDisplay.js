a17e903344a5fda64515157b0cb82cdc19da597d
thevikas <mevikas@gmail.com>
2024-03-24 17:42:37 +0530

    some DataTable navigation working

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -4,24 +4,77 @@ import { observer } from 'mobx-react-lite';
 import { dataStore } from './DataStore'; // Import your store
 import Config from "react-native-config";
 import { DataTable } from 'react-native-paper';
+import moment from 'moment';
+import { set } from 'mobx';
 
-function projectRow({ index, name, Dated, Income, isSummary }) {
+function projectRow({ index, name, Dated, Income, isSummary, project, onRowPress, onTaskRowPress, recent}) {
     boldText = isSummary ? { fontWeight: 'bold' } : {};
     Dated = isSummary ? "" : Dated;
+    const s1 = isSummary ? styles.summary : {}; 
+    const d2 = moment(Dated, "YYYY-MM-DD H:mm:ss").fromNow();
+    const projectCellStyle = project === name ? { 
+        fontWeight: 'bold',
+        backgroundColor: 'lightblue'
+     } : {};
     return (
-        <DataTable.Row key={index}>
-            <DataTable.Cell>{name}</DataTable.Cell>
-            <DataTable.Cell>{Dated}</DataTable.Cell>
-            <DataTable.Cell numeric>{Income}</DataTable.Cell>
+        <>
+            <DataTable.Row key={index} style={s1} onPress={() => onRowPress(name)}>
+                <DataTable.Cell style={projectCellStyle}>{name}</DataTable.Cell>
+                <DataTable.Cell style={projectCellStyle}>{d2}</DataTable.Cell>
+                <DataTable.Cell  style={projectCellStyle} numeric>{Income}</DataTable.Cell>
+            </DataTable.Row>
+            {project === name && projectTaskInfo(recent,onTaskRowPress)}
+        </>
+    );
+}
+
+function projectTaskInfo(recent,onTaskRowPress) {
+    console.log("L28 tasks",recent);
+    return (
+        <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title sortDirection='descending'>
+                        Task
+                    </DataTable.Title>
+                    <DataTable.Title numeric>Last time</DataTable.Title>
+                </DataTable.Header>
+                {(
+                    // Convert object values to an array and map over it
+                    Object.entries(recent).map(([key, item], index) => {
+                        return projectTaskRow({ index, ...item, onTaskRowPress})
+                    })
+                )}
+            </DataTable>
+    );
+}
+
+function projectTaskRow({ index, task, last_time, onTaskRowPress}) {
+    const d2 = moment(last_time, "YYYY-MM-DD H:mm:ss").fromNow();
+    return (
+        <DataTable.Row key={index} onPress={() => onTaskRowPress(name)}>
+            <DataTable.Cell>{task}</DataTable.Cell>
+            <DataTable.Cell numeric>{d2}</DataTable.Cell>
         </DataTable.Row>
     );
 }
 
 const DataDisplay = observer(() => {
+
+    const [project, setProject] = React.useState(null);
+
     useEffect(() => {
         console.log("howdy");
         //dataStore.fetchData(Config.API_URL + "/projects");
     }, []);
+
+    const onRowPress = (project) => {
+        setProject(project);
+        console.log("L35 open project",project);
+    }
+
+    const onTaskRowPress = (task) => {
+        console.log("L39 open task",task);
+    }
 
     return (
         <View style={styles.screenPad}>
@@ -40,7 +93,7 @@ const DataDisplay = observer(() => {
                     // Convert object values to an array and map over it
                     Object.entries(dataStore.data).map(([key, item], index) => {
                         isSummary = item.name === "Summary";
-                        return projectRow({ index, ...item, isSummary })
+                        return projectRow({ index, ...item, isSummary, project, onRowPress, onTaskRowPress})
                     })
                 )}
             </DataTable>
@@ -52,9 +105,11 @@ const styles = StyleSheet.create({
     screenPad: {
         padding: 10, // Add some padding to the screen
     },
-    line: {
+    summary: {
+        //bold
+        fontWeight: 'bold', // Make the text bold
         height: 1, // Height of the line
-        backgroundColor: '#000', // Color of the line
+        backgroundColor: 'gray', // Line color
         marginBottom: 5, // Space between the line and the summary row
     },
     container: {
